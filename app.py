6a1ababf1bca21b518013dcca4d0eeadaae4bde5
iyyappansurya <112376385+iyyappansurya@users.noreply.github.com>
2023-02-25 21:12:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, request, jsonify
 import requests
+import urllib.request as urllib2
 
 API_URL = "https://api-inference.huggingface.co/models/nickmuchi/yolos-small-plant-disease-detection"
-headers = {"Authorization": "Bearer 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"}
+headers = {"Authorization": "Bearer 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"}
 app = Flask(__name__)
 
 def query(data):
@@ -11,14 +12,20 @@ def query(data):
     response = requests.post(API_URL, headers=headers, data=data)
     return response.json()
 
-@app.route("/predict", methods=["POST"])
+@app.route("/")
+def index():
+    return "Hello World!"
+
+@app.route("/predict", methods=["GET"])
 def predict():
-    if request.method == "POST":
-        file = request.files["file"]
-        if file:
-            data = file.read()
-            print(data)
-            return jsonify(query(data))
+    urlimg2 = "https://firebasestorage.googleapis.com/v0/b/farmlabslogin.appspot.com/o/users%2FGzojrnYywSaIvLB8Q85Aijbc7O73%2Fuploads%2F"
+    if request.method == "GET":
+        img = request.args['img']
+        datas = img.split('/')
+        urlimg2 = urlimg2 + datas[-1]
+        urlimg2 = urlimg2 + '&token=' + request.args['token']
+    contents = urllib2.urlopen(urlimg2).read()
+    return jsonify(query(contents))
 
 if __name__ == "__main__":
     app.run(debug=True)
